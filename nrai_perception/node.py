6b99cdf5b72f19f09d1a3a5bf52053cce831bf4b
Gabriel Perry <93535957+GRP-Projects@users.noreply.github.com>
2026-06-07 17:22:55 +0100

    Reworked for rosless integration

---
+++ nrai_perception/node.py
@@ -0,0 +1,45 @@
+import os
+from code import ZEDYOLOTrack
+import pyzed.sl as sl
+
+fifo_path = '/opt/PERCEPTION_ZedYoloTrack'
+
+def main(args=None):
+    # --- Set up ZED Camera ---
+    zed = sl.Camera()
+    init_params = sl.InitParameters()
+    init_params.sdk_verbose=0
+    
+    if zed.open(init_params) > sl.ERROR_CODE.SUCCESS:
+        print("Unable to open ZED camera, aborting.")
+        exit(1)
+    
+    # --- Set up Code ---
+    node = ZEDYOLOTrack()
+    
+    # --- Set up IPC ---
+    os.mkfifo(fifo_path, 0o600)
+    
+    # --- Execute loop ---
+    image, depth = sl.Mat(), sl.Mat()
+    runtime_parameters = sl.RuntimeParameters()
+    while True:
+        if zed.grab(runtime_parameters):
+            zed.retrieve_image(image, sl.VIEW.LEFT)
+            zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+            
+            node.depth_callback(depth.get_data())
+            new_instruction = node.rgb_callback(image.get_data()).tobytes()
+            
+            # Write cone bytes
+            try:
+                fd = os.open(fifo_path, os.O_WRONLY)
+                with open(fd, "wb") as file:
+                    file.write(new_instruction)
+            except FileNotFoundError:
+                self.get_logger().info("Could not access FIFO. Likely not yet configured.")
+            except BrokenPipeError:
+                self.get_logger().info("FIFO terminated")
+
+if __name__ == "__main__":
+    main()
--- test/test_main.py
+++
@@ -1,5 +0,0 @@
-from nrai_perception.__main__ import main
-
-
-def test_main():
-    assert main() == 0
